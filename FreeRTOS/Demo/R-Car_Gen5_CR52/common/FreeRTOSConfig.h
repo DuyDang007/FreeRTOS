6b3e47a7b3a6f2a52edd780a7777c85b1a0adb8e
Phuong Le <phuong.le.wj@renesas.com>
2024-11-28 08:31:45 +0000

    Clean up and build freertos to static library

--- FreeRTOS/Demo/R-Car_Gen5_CR52/common/FreeRTOSConfig.h
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/common/FreeRTOSConfig.h
@@ -16,7 +16,6 @@
 #define configSUPPORT_STATIC_ALLOCATION                 1
 #define configSUPPORT_DYNAMIC_ALLOCATION                1 /* Defaults to 1 anyway. */
 
-// Phuong
 #define __BKPT(value)                       __ASM volatile ("bkpt "#value)
 #define __ASM                                  __asm
 
