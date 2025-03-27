58f5a6f3d5bf56c7606ca552f301d6815e6fe568
huybui2 <huy.bui.wm@renesas.com>
2025-03-27 20:56:39 +0700

    INTC: Fix merge interrupt setup
    Signed-off-by: huybui2 <huy.bui.wm@renesas.com>

--- FreeRTOS/Demo/R-Car_Gen5_CR52/common/interrupts.c
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/common/interrupts.c
@@ -206,8 +206,12 @@ void Irq_SetupEntry(unsigned int id, IrqHandlerFn Handler, Context_t *Context)
 			;
 
 	ret = Irq_MergeSetup(id);
-	// if(!ret)
-	// 	printf("Merged Interrupt setup completed.\n");
+
+    /* Work around: Wait for merger interrupt setup */
+    for (int i = 0; i < 1000000; i++)
+    {
+        ;
+    }
 
 	HandlerTable[id].Handler = Handler;
 	HandlerTable[id].Context = Context;
