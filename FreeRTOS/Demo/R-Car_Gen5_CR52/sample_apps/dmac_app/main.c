2f0cd71496c2b0721424c78e822b42b043966619
Thanh Quan <thanh.quan.xn@renesas.com>
2025-04-18 13:20:58 +0700

    dmac: Add sample app with DMA descriptor nomal mode
    This patch is used to add sample application with DMA descriptor nomal
    mode.
    This patch also adds sample applicaiton with DMA descriptor repeat
    mode but we haven't verified the operation yet.
    
    Signed-off-by: Thanh Quan <thanh.quan.xn@renesas.com>

--- FreeRTOS/Demo/R-Car_Gen5_CR52/sample_apps/dmac_app/main.c
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/sample_apps/dmac_app/main.c
@@ -51,8 +51,9 @@ static void prvSetupHardware( void );
 
 static void prvDMACTask( void *pvParameters );
 
+/*------------------------- Configure mem-to-mem with Normal mode ----------------------------------*/
 /* Define configure DMA Controller */
-rDmacCfg_t cfg =
+rDmacCfg_t cfg0 =
 {
 	//Fill in the configuration details
 	.mSrcAddr = 0x189E7000,
@@ -66,6 +67,63 @@ rDmacCfg_t cfg =
 	.mLowSpeed = DRV_RTDMAC_SPEED_NORMAL, // Assuming DRV_RTDMAC_SPEED_NORMAL is defined
 	.mPrioLevel = 0
 };
+
+/*------------------------- Configure mem-to-mem with Descriptor Normal mode ----------------------------------*/
+rDmacCfg_t cfg1 =
+{
+	//Fill in the configuration details
+	.mSrcAddr = 0x189E7000,
+	.mDestAddr = 0x189E7100,
+	.mTransferCount = 3,
+	.mDMAMode = DRV_DMAC_DMA_DESC_NORMAL, // Assuming DRV_DMAC_DMA_DESC_NORMAL is defined
+	.mSrcAddrMode = DRV_RTDMAC_ADDR_FIXED, // Assuming ADDR_MODE_FIXED is defined
+	.mDestAddrMode = DRV_RTDMAC_ADDR_FIXED, // Assuming ADDR_MODE_FIXED is defined
+	.mTransferUnit = DRV_RTDMAC_TRANS_UNIT_4BYTE, // Assuming DRV_RTDMAC_TRANS_UNIT_4BYTE is defined
+	.mResource = DRV_RTDMAC_MEMORY, // Assuming DRV_RTDMAC_MEMORY is defined
+	.mLowSpeed = DRV_RTDMAC_SPEED_NORMAL, // Assuming DRV_RTDMAC_SPEED_NORMAL is defined
+	.mPrioLevel = 0
+};
+
+/* Define configure the DMA descriptor */
+rDmacDescCfg_t descCfg1 =
+{
+	.mDescBaseAddr = 0x189E7000,
+	.mDescUpdate = {0},
+	.mDescRead1st = true,
+	.mStateEndEnable = false,
+	.mDescCount = 3,
+	.mDescIndex = 0
+};
+
+/*------------------------- Configure mem-to-mem with Descriptor Repeat mode ----------------------------------*/
+
+/* Define configure DMA Controller */
+rDmacCfg_t cfg2 =
+{
+    .mSrcAddr = 0x189E7000,                     // Source address
+    .mDestAddr = 0x189E7100,                    // Destination address
+    .mTransferCount = 4,                        // Transfer count
+    .mDMAMode = DRV_DMAC_DMA_DESC_REPEAT,       // Reapeat descriptor mode
+    .mSrcAddrMode = DRV_RTDMAC_ADDR_FIXED,      // Source address fixed
+    .mDestAddrMode = DRV_RTDMAC_ADDR_INCREMENTED, // Destination address increment
+    .mTransferUnit = DRV_RTDMAC_TRANS_UNIT_4BYTE, // Transfer unit 4 bytes
+    .mResource = DRV_RTDMAC_MEMORY,            // DMA resource
+    .mLowSpeed = DRV_RTDMAC_SPEED_NORMAL,
+    .mPrioLevel = 1
+};
+
+/* Define configure the DMA descriptor */
+rDmacDescCfg_t descCfg2 =
+{
+    .mDescBaseAddr = 0x189E7200,                // Descriptor base address
+    .mDescUpdate = {0},                         // No descriptor update
+    .mDescRead1st = true,                       // Read descriptor first
+    .mStateEndEnable = true,                    // Trigger state end
+    .mDescCount = 2,                            // Descriptor count
+    .mDescIndex = 0                             // Descriptor index
+};
+
+/*-----------------------------------------------------------*/
 
 rDmacIrqCfg_t rDmacIrqHandler_t_irq =
 {
@@ -109,29 +167,29 @@ static void prvDMACTask( void *pvParameters )
 	( void ) pvParameters;
 	int ret;
 
-    Context_t usr_context = 
-    {
-        .ctx = &rDmacIrqHandler_t_irq,
-    };
+	Context_t usr_context =
+	{
+		.ctx = &rDmacIrqHandler_t_irq,
+	};
 
 	/* Device Driver Part */
 	R_SYSDMAC_RcarDmacCtrlInit(SYS_DMAC3, DRV_RTDMAC_PRIO_FIX);
 
-	*(volatile uint32_t*)cfg.mSrcAddr = 0x479;
-	printf_delay("Value at SrcAddr: 0x%x \n",*(volatile uint32_t*)cfg.mSrcAddr);
+	*(volatile uint32_t*)cfg1.mSrcAddr = 0x3;
+	printf_delay("Value at SrcAddr: 0x%x \n",*(volatile uint32_t*)cfg1.mSrcAddr);
 
 	ret = R_SYSDMAC_RcarCallBackSet(&rDmacIrqHandler_t_irq, dmacUserCallback, &usr_context);
 	if (ret)
 		printf_delay("CallbackSet Failed: ret = %d\n", ret);
 
-	int dmaStatus =R_SYSDMAC_RcarDmacExec(SYS_DMAC3, DMAC_CH1, &cfg, 0);
+	int dmaStatus =R_SYSDMAC_RcarDmacExec(SYS_DMAC3, DMAC_CH1, &cfg1, &descCfg1);
 
 	// Check DMA execution status
 	if (dmaStatus != 0)
 		printf_delay("DMA execution failed with status: %d\n", dmaStatus);
 
 	// Verify destination data
-	uint32_t destData = *(volatile uint32_t*)cfg.mDestAddr;
+	uint32_t destData = *(volatile uint32_t*)cfg1.mDestAddr;
 	printf_delay("Value at DestAddr after DMA: 0x%x \n", destData);
 
 	for( ;; )
