6d16067165cd3b4bbe0b00d73e4f98bc405de80e
Phuong Le <phuong.le.wj@renesas.com>
2025-06-06 12:59:25 +0000

    [Gen5][Timer]: Reduce timer frequency for simulation system.

--- FreeRTOS/Demo/R-Car_Gen5_CR52/drivers/timer/arm_generic_timer.h
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/drivers/timer/arm_generic_timer.h
@@ -21,7 +21,7 @@
 #else
 /* The timer count up 16 counts with 66.667MHz clock.
 It means the counter operate at 1066.667MHz in equivalent */
-#define GENERIC_TIMER_CLK     1066666666    // Hz
+#define GENERIC_TIMER_CLK     25000000    // This is workaround for simulation system.
 
 #endif
 
