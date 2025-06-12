18b2fb19ac2c09e9c0276f2020ef69e161de548f
Huy Bui <huy.bui.wm@renesas.com>
2025-06-12 15:39:54 +0700

    [Gen5][PFC]: Enable for I2C0 to I2C8
    Enable PFC for I2C0 to I2C8
    
    Signed-off-by: Huy Bui <huy.bui.wm@renesas.com>

--- FreeRTOS/Demo/R-Car_Gen5_CR52/include/device_tree_x5h.h
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/include/device_tree_x5h.h
@@ -18,19 +18,26 @@
  * @brief Superset list of all HW IPs.
  */
 typedef enum {
-    MODULE_INVALID = 0,		///< Invalid module ID.
-    MODULE_SCIF0,			///< Module ID: SCIF0.
-    MODULE_SCIF1,			///< Module ID: SCIF1.
-    MODULE_SCIF2,			///< Module ID: SCIF2.
-    MODULE_SCIF3,			///< Module ID: SCIF3.
-    MODULE_HSCIF0,			///< Module ID: HSCIF0.
-    MODULE_HSCIF1,			///< Module ID: HSCIF1.
-    MODULE_HSCIF2,			///< Module ID: HSCIF2.
-    MODULE_HSCIF3,			///< Module ID: HSCIF3.
-    MODULE_I2C0,			///< Module ID: I2C0.
-    MODULE_I2C1,			///< Module ID: I2C1.
+    MODULE_INVALID = 0,     ///< Invalid module ID.
+    MODULE_SCIF0,           ///< Module ID: SCIF0.
+    MODULE_SCIF1,           ///< Module ID: SCIF1.
+    MODULE_SCIF2,           ///< Module ID: SCIF2.
+    MODULE_SCIF3,           ///< Module ID: SCIF3.
+    MODULE_HSCIF0,          ///< Module ID: HSCIF0.
+    MODULE_HSCIF1,          ///< Module ID: HSCIF1.
+    MODULE_HSCIF2,          ///< Module ID: HSCIF2.
+    MODULE_HSCIF3,          ///< Module ID: HSCIF3.
+    MODULE_I2C0,            ///< Module ID: I2C0.
+    MODULE_I2C1,            ///< Module ID: I2C1.
+    MODULE_I2C2,            ///< Module ID: I2C2.
+    MODULE_I2C3,            ///< Module ID: I2C3.
+    MODULE_I2C4,            ///< Module ID: I2C4.
+    MODULE_I2C5,            ///< Module ID: I2C5.
+    MODULE_I2C6,            ///< Module ID: I2C6.
+    MODULE_I2C7,	    ///< Module ID: I2C7.
+    MODULE_I2C8,            ///< Module ID: I2C8.
     // Add device_id
-    MODULE_MAX = 255			///< Max module ID.
+    MODULE_MAX = 255        ///< Max module ID.
 } e_module_id_t;
 
 #if (BOARD == X5H_VDK)
@@ -38,6 +45,11 @@ typedef enum {
     MODULE_CONFIG(MODULE_SCIF1, 1), \
     MODULE_CONFIG(MODULE_I2C0,  1), \
     MODULE_CONFIG(MODULE_I2C1,  1), \
+    MODULE_CONFIG(MODULE_I2C2,  1), \
+    MODULE_CONFIG(MODULE_I2C3,  1), \
+    MODULE_CONFIG(MODULE_I2C4,  1), \
+    MODULE_CONFIG(MODULE_I2C5,  1), \
+    MODULE_CONFIG(MODULE_I2C6,  1),  \
     /* Add module configs here */   \
     /* Sentinel guard: Do not remove */ \
     MODULE_CONFIG(MODULE_INVALID, 0) \
