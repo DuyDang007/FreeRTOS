9aebe7ad797e9603c53bbad9ef61e13aab6d13b1
Phuc Le <phuc.le.wx@renesas.com>
2025-07-23 15:30:25 +0700

    x5h: memmap: using RAM_NOCACHE_ATTR for Linux_CMA & SHARE_CR_CA address
    Signed-off-by: Phuc Le <phuc.le.wx@renesas.com>

--- FreeRTOS/Demo/R-Car_Gen5_CR52/common/memory_map/memory_map_x5h_ironhide.h
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/common/memory_map/memory_map_x5h_ironhide.h
@@ -44,8 +44,8 @@ typedef struct st_memory_region {
 
 static const st_memory_region_t RCAR_MEMMORY_ARR[] = {
     {.type = OSAL,          .mem_addr = {.base_address = (uint32_t) OSAL_MEMORY_ADDRESS, .size = (uint32_t) OSAL_MEMORY_SIZE},    .attr = RAM_ATTR},
-    {.type = SHARE_MEM,     .mem_addr = {.base_address = (uint32_t) LINUX_CMA_ADDRESS_0, .size = (uint32_t) LINUX_CMA_SIZE_0},    .attr = RAM_ATTR},
-    {.type = SHARE_MEM,     .mem_addr = {.base_address = (uint32_t) SHARE_CR_CA_ADDRESS, .size = (uint32_t) SHARE_CR_CA_SIZE},    .attr = RAM_ATTR},
+    {.type = SHARE_MEM,     .mem_addr = {.base_address = (uint32_t) LINUX_CMA_ADDRESS_0, .size = (uint32_t) LINUX_CMA_SIZE_0},    .attr = RAM_NOCACHE_ATTR},
+    {.type = SHARE_MEM,     .mem_addr = {.base_address = (uint32_t) SHARE_CR_CA_ADDRESS, .size = (uint32_t) SHARE_CR_CA_SIZE},    .attr = RAM_NOCACHE_ATTR},
     {.type = PERIPHERAL,    .mem_addr = {.base_address = (uint32_t) PERIPHERAL_START_0,  .size = (uint32_t) PERIPHERAL_SIZE_0},   .attr = DEVICE_ATTR},
     {.type = PERIPHERAL,    .mem_addr = {.base_address = (uint32_t) PERIPHERAL_START_1,  .size = (uint32_t) PERIPHERAL_SIZE_1},   .attr = DEVICE_ATTR}
 };
