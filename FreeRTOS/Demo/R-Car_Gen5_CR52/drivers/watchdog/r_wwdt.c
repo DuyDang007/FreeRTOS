a2f990dca6b0ca6aba63c60b210939f1a68252a0
Minh Le <minh.le.aj@renesas.com>
2025-09-28 22:07:47 +0700

    wwdt: update clock handling for WWDT module
    Due to lack of controlling the clock input,
    WWDT cannot work normally. This patch adds to fix
    this issue.
    
    Signed-off-by: Minh Le <minh.le.aj@renesas.com>

--- FreeRTOS/Demo/R-Car_Gen5_CR52/drivers/watchdog/r_wwdt.c
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/drivers/watchdog/r_wwdt.c
@@ -10,6 +10,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "watchdog/r_wwdt_api.h"
+#include "state-manager/r_clock_domain_id.h"
+#include "state-manager/r_state_manager.h"
 #include "r_wwdt_reg.h"
 
 const uint16_t timeout_ch0_19[] = { 15, 31, 62, 124, 250, 500, 1000, 2000 };
@@ -49,6 +51,135 @@ void R_WWDT_Init(wwdt_unit_t unit, wwdt_wsize_t wsize, uint32_t timeout_msec, bo
 {
 	uint8_t val;
 	uintptr_t wwdt_base_addr = R_WWDT_PRV_GetRegbase(unit);
+	int clock_id, clock_id_0, clock_id_1, ret;
+
+	clock_id = X5H_CLOCK_ID_MDLC_WDT0;
+	ret = R_StateManager_ClockOn(clock_id);
+	if (ret)
+		printf("Error: Failed to set clock id %d ON.\r\n", clock_id);
+
+	switch(unit) {
+		case R_WWDT0:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT00;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT01;
+			break;
+		case R_WWDT1:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT10;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT11;
+			break;
+		case R_WWDT2:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT20;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT21;
+			break;
+
+		case R_WWDT3:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT30;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT31;
+			break;
+
+		case R_WWDT4:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT40;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT41;
+			break;
+
+		case R_WWDT5:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT50;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT51;
+			break;
+
+		case R_WWDT6:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT60;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT61;
+			break;
+
+		case R_WWDT7:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT70;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT71;
+			break;
+
+		case R_WWDT8:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT80;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT81;
+			break;
+
+		case R_WWDT9:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT90;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT91;
+			break;
+
+		case R_WWDT10:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT100;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT101;
+			break;
+
+		case R_WWDT11:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT110;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT111;
+			break;
+
+		case R_WWDT12:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT120;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT121;
+			break;
+
+		case R_WWDT13:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT130;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT131;
+			break;
+
+		case R_WWDT14:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT140;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT141;
+			break;
+
+		case R_WWDT15:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT150;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT151;
+			break;
+
+		case R_WWDT16:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT160;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT161;
+			break;
+
+		case R_WWDT17:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT170;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT171;
+			break;
+
+		case R_WWDT18:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT180;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT181;
+			break;
+
+		case R_WWDT19:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT190;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT191;
+			break;
+
+		case R_WWDT20:
+			clock_id_0 = X5H_CLOCK_ID_MDLC_WWDT200;
+			clock_id_1 = X5H_CLOCK_ID_MDLC_WWDT201;
+			break;
+		default:
+			printf("Wrong Unit for Clock ID\n");
+			break;
+		}
+
+       ret = R_StateManager_ResetAssert(clock_id_0);
+       if (ret)
+               printf("Error: Failed to reset clock id %d.\r\n", clock_id_0);
+       ret = R_StateManager_ResetAssert(clock_id_1);
+       if (ret)
+               printf("Error: Failed to reset clock id %d.\r\n", clock_id_1);
+
+       ret = R_StateManager_ResetDeassert(clock_id_0);
+       if (ret)
+               printf("Error: Failed to DeassertReset clock id %d.\r\n", clock_id_0);
+
+       ret = R_StateManager_ResetDeassert(clock_id_1);
+       if (ret)
+               printf("Error: Failed to DeassertReset clock id %d.\r\n", clock_id_1);
 
 	clk_rate = (wwdt_base_addr == 0xC1380000) ? CLK_LSIOSC : RCLK;
 	val = r_wwdt_read(wwdt_base_addr + WDTA0MD);
