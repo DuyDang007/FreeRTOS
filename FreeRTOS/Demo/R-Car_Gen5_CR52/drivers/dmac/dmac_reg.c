995566e7dec3029411273d515fc59b93d6aecc61
Nghia Hoang <nghia.hoang.hx@bp.renesas.com>
2025-10-02 16:16:15 +0700

    x5h: dmac: Correct step configuration in DMAC descriptor.
    Signed-off-by: Nghia Hoang <nghia.hoang.hx@bp.renesas.com>

--- FreeRTOS/Demo/R-Car_Gen5_CR52/drivers/dmac/dmac_reg.c
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/drivers/dmac/dmac_reg.c
@@ -547,7 +547,7 @@ void R_RTDMAC_Update_RDMCHCRB(uint8_t dev, uint8_t ch, rDmacCfg_t *cfg, rDmacDes
     rRDMCHCRB.bit.SLM = cfg->mLowSpeed;
     rRDMCHCRB.bit.PRI = cfg->mPrioLevel;
     if (DRV_DMAC_DMA_NO_DESCRIPTOR != cfg->mDMAMode) {
-        rRDMCHCRB.bit.DCNT = descCfg->mDescCount;
+        rRDMCHCRB.bit.DCNT = descCfg->mDescCount - 1;
     }
     R_RTDMAC_WriteReg32(dev, ch, RDMCHCRB, rRDMCHCRB.INT);
 }
